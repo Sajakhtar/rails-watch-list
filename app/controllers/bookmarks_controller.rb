b416aa956adac8a03da43112ff6f2a904bda7efc
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-11 12:51:15 +0400

    routes, controler  methods for list and bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,33 @@
 class BookmarksController < ApplicationController
   def new
+    @bookmark = Bookmark.new
+    @movie = Movie.new
+  end
+
+  def create
+    @bookmark = Review.new(bookmark_params)
+    @movie = Movie.find(params[:movie_id])
+    @list = List.find(params[:list_id])
+    @bookmark.movie = @movie
+
+    if @bookmark.save
+      redirect_to @movie
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path, notice: 'Bookmark was successfully destroyed.'
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:name)
   end
 end

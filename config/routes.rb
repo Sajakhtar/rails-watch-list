a1f3805adb579617c23574ac42af985a068c5a66
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-11 12:06:22 +0400

    routes and views for lists and bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :boomarks, only: [:new, :create]
+  end
+
+  resources :boomarks, only: [:delete]
 end
